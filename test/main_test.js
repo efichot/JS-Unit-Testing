d74f25dc145c6e016074e4269c63e7d4eade2d68
Etienne FICHOT <efichot@e2r12p19.42.fr>
2017-07-12 20:44:02 +0200

    test suite for grp unit test

---
+++ test/main_test.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+
+describe('checkForChip', function() {
+    const checkForShip = require('../game_logic/ship_methods').checkForShip;
+    it('should correctly report no ship at a given players coordinate', function() {
+        
+    })
+});
